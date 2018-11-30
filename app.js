fbfa17d10841a979ee48c540316c392e5f29a715
Joseph Noott <Jnootty@hotmail.com>
2018-11-30 18:00:52 +0100

    -a function added/!

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to fetch weather for',
+      string: true
+    }
+})
+  .help()
+  .alias('help', 'h')
+  .argv;
+
+var encodedAddress = encodeURIComponent(argv.address);
+
 
 request ({
-  url: 'http://www.mapquestapi.com/geocoding/v1/address?key=XG9GRB2ApJofK41OMDUOeajP5xXFzjRy&location=1301%20lombard%20street%20philadelphia',
+  url: `http://www.mapquestapi.com/geocoding/v1/address?key=XG9GRB2ApJofK41OMDUOeajP5xXFzjRy&location=${encodedAddress}`,
   json: true
 }, (error, response, body) => {
   console.log(`Address: ${body.results[0].formatted_address}`);

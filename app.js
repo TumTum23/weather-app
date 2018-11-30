6f4f88a96ef72147acdfd25b71bb68b4961dab19
Joseph Noott <Jnootty@hotmail.com>
2018-11-30 16:27:23 +0100

    API working

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const request = require('request');
 
 request ({
-  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
+  url: 'http://www.mapquestapi.com/geocoding/v1/address?key=XG9GRB2ApJofK41OMDUOeajP5xXFzjRy&location=1301%20lombard%20street%20philadelphia',
   json: true
 }, (error, response, body) => {
-  console.log(body);
+  console.log(`Address: ${body.results[0].formatted_address}`);
+  console.log(`Lattitude: ${body.results[0].locations[0].latLng.lat}`);
+  console.log(`Longitude: ${body.results[0].locations[0].latLng.lng}`);
 });
